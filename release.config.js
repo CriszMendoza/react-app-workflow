7569e8b3825f0cd3cb86f247df03e4c5034947a6
Crisz Mendoza <criszmendoza212@gmail.com>
2022-10-10 17:05:08 -0500

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/CriszMendoza/react-app-workflow",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
