79766787e5d3c779835ddd2074e46f503c32258d
Crisz Mendoza <criszmendoza212@gmail.com>
2022-10-11 17:02:01 -0500

    ci(sadsadasd): asdasdsadas asdasd

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,7 +2,13 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
+  core.debug("Debuge message");
+  core.debug("Warning message");
+  core.debug("Error message");
+
   const name = core.getInput("who-to-greet");
+
+  core.setSecret(name);
 
   console.log(`Hello ${name}`);
 
@@ -10,7 +16,11 @@ try {
 
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
