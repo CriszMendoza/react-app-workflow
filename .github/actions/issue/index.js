88db3fa98702037c6001e9adb9d7556c3548579a
Crisz Mendoza <criszmendoza212@gmail.com>
2022-10-12 17:21:25 -0500

    ci(hello-docker): cacsacas
    cascsacacascas

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,15 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-//  curl --request POST \
-//    --url https://api.github.com/repos/${{ github.repository }}/issues \
-//    --header 'authorization: Bearer ${{ secrets.GITHUB_TOKEN }}' \
-//    --data '{
-//      "title": "Automated issue for commit: ${{ github.sha }}",
-//      "body": "THis issue was automatically created by the Github Action workflow **${{ github.workflow}}**. \n\n The commit hash was: ${{ github.sha }}_.",
-//      "assignees": ["${{ github.event.pull_request.user.login }}"]
-//      }'
-
 (async () => {
   try {
     const token = core.getInput("token");
